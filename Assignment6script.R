f904f49a3ec40c428d25db329fe19f862dded1ac
William Ellis <wellis@usf.edu>
2023-02-20 07:45:39 -0500

    version2

--- Assignment6script.R
+++ Assignment6script.R
@@ -6,3 +6,13 @@ SAB=A+B #Find sum of matrices A and B
 print(SAB)
 DAB=A-B #Find the difference of matrices A and B
 print(DAB)
+
+m=c(4,1,2,3) #create vector with specified values
+M=diag(m,4) #create matrix with vector values in diagonal
+print(M)
+
+N=(diag(5))*3
+O=matrix(c(0,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0,1,0,0,0,0), ncol=5)
+P=matrix(c(0,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0), ncol=5)
+Q=N+O+P
+print(Q)
